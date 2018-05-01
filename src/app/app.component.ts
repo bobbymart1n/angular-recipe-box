180b2077d7b37cd7e7b4de173b22aafe550be45e
Bobby Martin and Isaac Shoman <isaac@>
2018-05-01 09:48:18 -0700

    Recipes app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+  recipes: Recipe[] = [
+    new Recipe('Sugar Cookie', ['flour', 'sugar', 'milk'], ['Preheat oven to 375', 'mix ingredients in bowl', 'pour into other bowl', 'get baked']),
+    new Recipe('Bread', ['flour', 'salt', 'water', 'egg'], ['Preheat oven to 375', 'mix ingredients in bowl', 'pour into other bowl', 'get baked']),
+    new Recipe('Delicious Coffee', ['coffee', 'water'], ['Heat water to 200 degrees', 'grind coffee to salt like granuels', 'cover grounds with hot water', 'wait 30 seconds to bloom coffee', 'pour remaining water in circular motion over grounds'])
+  ];
+
+  constructor() {}
 }
